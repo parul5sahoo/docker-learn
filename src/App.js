d0f2e0bd4b323e96a1f13ac6c8e6ce88e641c5fe
Parul Sahoo <parulsahoo5jan@gmail.com>
2021-04-17 22:10:15 +0530

    chnaged app text again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Changed on feature branch!!
+         Changed the text of app on feature branch!!
         </p>
         <a
           className="App-link"
